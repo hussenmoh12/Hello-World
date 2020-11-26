b6db4c1a3d1e26b6b0e67f2608aeab573ab6cb25
hussenmoh12 <191700053@ecu.edu>
2020-11-26 11:17:03 +0200

    FirstTry commit1

--- First/Source.cpp
+++ First/Source.cpp
@@ -3,11 +3,15 @@
 using namespace std;
 void main()
 {
-	string name; int age; string address;
+	string name; int age; string address; string university;
 	cout << "What is your name and address" << endl;
 	getline(cin, name); getline(cin, address);
 	cout << "enter your age" << endl;
 	cin >> age;
+	//FirstTry
+	cout << "What is your university" << endl;
+	getline(cin, university);
+
 
 
 }
