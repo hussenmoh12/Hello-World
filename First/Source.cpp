3ba2a7801db2fb1a1a4384663e4cc6744194b0ad
Hussen Mohamed <191700053@ecu.edu.eg>
2020-12-17 12:05:19 +0200

    added more information

--- First/Source.cpp
+++ First/Source.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 void main()
 {
-	string name; int age; string address; string university;
+	string name; int age; string address; string university; string job;
 	cout << "What is your name and address" << endl;
 	getline(cin, name); getline(cin, address);
 	cout << "enter your age" << endl;
@@ -12,6 +12,8 @@ void main()
 	cout << "What is your university" << endl;
 	getline(cin, university);
 
-
+	//More information
+	cout << "enter your job" << endl;
+	getline(cin, job);
 
 }
